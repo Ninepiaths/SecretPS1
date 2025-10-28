940e8da6027cc509b155e5df8c117f879afa896a
Ninepiaths <102284599+Ninepiaths@users.noreply.github.com>
2025-10-28 12:06:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.use(function (req, res, next) {
 });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/images'));
 //app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 app.all('/favicon.ico', function(req, res) {
     
